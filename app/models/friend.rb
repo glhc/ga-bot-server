e55afed5c08243a5e86ae97549b9aa0033267024
Patrick Horne <patrickahorne@gmail.com>
2019-12-10 20:48:24 +1100

    Attempt to establish user/friend/relationships

---
+++ app/models/friend.rb
@@ -0,0 +1,4 @@
+class Friend < ActiveRecord::Base
+  belongs_to :user, :class_name => "User"
+  belongs_to :friend, :class_name => "Friend"
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :friends,
+    :foreign_key => "friend_id",
+    :class_name => "Friend",
+    :dependent => :destroy
+
+  has_many :relationships,
+    :foreign_key => "friend_id",
+    :class_name => "Relationship",
+    :dependent => :destroy
 end
