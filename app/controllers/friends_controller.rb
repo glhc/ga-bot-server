a3067b092a20a21a1fca76bf6e858e6475762e65
53417 <seanimal@Live.com.au>
2019-12-14 11:56:34 +1100

    more endpoints

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -11,25 +11,42 @@ class FriendsController < ApplicationController
     # GET /friends
     # GET /friends.json
     def index
-        @friends = Friend.all
-        render json: @friends
-    end
-
-    # POST /friend_add
-    # POST /friend_add.json
-    def create
-        @friend = Friend.new(friend_params)
-        @friend.save
-    end
-
-    # DELETE /friend_delete/1
-    # DELETE /friend_delete/1.json
-    def destroy
-        @friend = Friend.find_by :id => params[:id]
-        respond_to do |format|
-            format.html { redirect_to friend_url, notice: 'Friend deleted' }
-            format.json { head :no_content }
+        logged_in = 1
+        @friends = Friend.where(user_id: logged_in)
+        @friends_detail = []
+        @friends.each do |friend|
+            @friends_detail.push(User.find_by :id => friend.friend_id)
         end
+        render json: @friends_detail
+    end
+
+    def read_followers
+        logged_in = 1
+        @following = Friend.where(friend_id: logged_in)
+        @follower_detail = []
+        @following.each do |friend|
+            @follower_detail.push(User.find_by :id => friend.friend_id)
+        end
+        render json: @follower_detail
+    end
+
+    def read_following
+        logged_in = 1
+        @following = Friend.where(user_id: logged_in)
+        @following_detail = []
+        @following.each do |friend|
+            @following_detail.push(User.find_by :id => friend.friend_id)
+        end
+        render json: @following_detail
+    end
+
+    def destroy_following
+        
+    end
+
+    def read_people
+        @people = User.all
+        render json: @people
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :users
   
   get '/friends' => 'friends#index'
+  get '/read_followers' => 'friends#read_followers'
+  get '/read_following' => 'friends#read_following'
+  post '/destroy_following' => 'friends#destroy_following'
+  get '/read_people' => 'friends#read_people'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
