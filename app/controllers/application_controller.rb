6914a26d99096132bba93e5db216b954ab8412a0
Patrick Horne <patrickahorne@gmail.com>
2019-12-14 14:59:33 +1100

    auth update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Knock::Authenticable
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user
 end
