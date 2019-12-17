676ac05bbd1dde2a0cf6cceef22a0c8220ba1787
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-12-17 23:00:18 +1100

    seoh1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+<<<<<<< HEAD
+=======
+require faker
+>>>>>>> seeds fix
 include BCrypt
 
 # # Creates a user with a random name
